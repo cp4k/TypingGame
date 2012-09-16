326dfd2789fb8d580a0b9c751e57cd7618bf33d4
Carter Sande <cp4khelp@gmail.com>
2012-09-15 20:43:56 -0700

    Initial basic typing game

---
+++ TypingGame.py
@@ -0,0 +1,42 @@
+import pygame, random
+pygame.init()
+
+width,height = (640,480)
+screen = pygame.display.set_mode((width,height))
+
+class TypingGameWord(pygame.sprite.Sprite):
+    def __init__(self, word):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.Font(None,100)
+        self.originalWord = word
+        self.word = word
+        self.updateSurface()
+    def checkLetter(self, letter):
+        "Checks a letter that the player typed.  Returns true if the word is empty, otherwise false."
+        if letter == self.word[0]:
+            self.word = self.word[1:]
+            self.updateSurface()
+        return self.word == ""
+    def updateSurface(self):
+        self.image = self.font.render(self.word, True, (255,255,255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (320,240)
+
+running = True
+
+word= TypingGameWord("Test!") #set our initial word
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            else:
+                if word.checkLetter(event.unicode): #event.unicode is the letter the user typed
+                    word = TypingGameWord("Another test!")
+    screen.fill((0,0,0))
+    screen.blit(word.image, word.rect)
+    pygame.display.flip()
+
+pygame.quit()
